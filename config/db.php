f1d1ac04131034cdbfd074085430a19a5c8c167e
iraoui365 <iraoui.e365@ucd.ac.ma>
2026-04-04 18:53:41 +0100

    make button smaller

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'gestion_besoins';
-$user = 'root';    
-$pass = '';         
+// Fichier : config/db.php
+
+// On inclut les variables secrètes
+/*require_once __DIR__ . '/config.php';      
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur connexion BD : " . $e->getMessage());
-}
+}*/
