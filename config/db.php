e86efba592b82ae4749124ef0947f21ef8dcd961
iraoui365 <iraoui.e365@ucd.ac.ma>
2026-04-04 19:47:16 +0100

    Retrait de config.php du suivi Git

--- config/db.php
+++ config/db.php
@@ -1,2 +1,14 @@
+<?php
+// Fichier : config/db.php
+
+// On inclut les variables secrètes
+require_once __DIR__ . '/config.php';
+
+try {
+	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	die("Erreur connexion BD : " . $e->getMessage());
+}
 
 
