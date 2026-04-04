a088e0aad65c0efbd55250aebb612370af5e17d4
iraoui365 <iraoui.e365@ucd.ac.ma>
2026-04-04 19:09:20 +0100

    somthing

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 // Fichier : config/config.php
 // NE PAS ENVOYER SUR GITHUB
 
@@ -6,17 +6,3 @@ $host = 'host';
 $dbname = 'eliraoui_gdeb';
 $user = 'eliraoui';
 $pass = 'iraoui0101';
-
-
-<?php
-// Fichier : config/db.php
-
-// On inclut les variables secrètes
-require_once __DIR__ . '/config.php';      
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur connexion BD : " . $e->getMessage());
-}
